0367e9d794ace90db0fce393f3860baec4c5c90b
Natalie Lukaschyk <N.Janke@live.de>
2024-08-20 12:15:06 +0200

    basic structure, loginform

---
+++ src/app/books/page.js
@@ -0,0 +1,9 @@
+export default function Books() {
+  return (
+    <main>
+      <div>
+        <p>I am the Books page</p>
+      </div>
+    </main>
+  );
+}
---
+++ src/app/profile/page.js
@@ -0,0 +1,9 @@
+export default function Profile() {
+  return (
+    <main>
+      <div>
+        <p>I am the Profiles page</p>
+      </div>
+    </main>
+  );
+}
